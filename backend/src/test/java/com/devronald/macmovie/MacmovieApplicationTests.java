dd3bf86a137be22670c84a5dca296037912c1e45
Ronald Joaquim <ronaldjoaquimcardoso11@gmail.com>
2022-01-10 15:39:41 -0300

    Project Created

---
+++ backend/src/test/java/com/devronald/macmovie/MacmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devronald.macmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
