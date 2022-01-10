8d6fe23e755ec009b5a7e28004125619c7c9a37d
Ronald Joaquim <ronaldjoaquimcardoso11@gmail.com>
2022-01-10 19:39:30 -0300

    Form layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,42 @@
-function Form (){
+import './styles.css';
+
+function Form() {
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
     return (
-        <h1>Página de Formulario</h1>
+        <div className="mecmovie-form-container">
+            <img className="mecmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="mecmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="mecmovie-form">
+                    <div className="form-group mecmovie-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group mecmovie-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="mecmovie-form-btn-container">
+                        <button type="submit" className="btn btn-primary mecmovie-btn">Salvar</button>
+                    </div>
+                </form >
+                <button className="btn btn-primary mecmovie-btn mt-3">Cancelar</button>
+            </div >
+        </div >
     );
 }
 
