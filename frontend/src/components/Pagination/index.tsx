0656dfb73438c9e42760a433b488058ffe8e891c
Ronald Joaquim <ronaldjoaquimcardoso11@gmail.com>
2022-01-15 15:21:43 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,15 +1,24 @@
 import { ReactComponent as Arrow } from 'assets/img/arrow.svg';
+import { MoviePage } from 'types/movie';
 import './styles.css';
 
-function Pagination() {
+type Props = {
+    page: MoviePage;
+    onChange: Function
+}
+
+function Pagination( { page, onChange } : Props) {
+
     return (
         <div className="mecmovie-pagination-container">
             <div className="mecmovie-pagination-box">
-                <button className="mecmovie-pagination-button" disabled={true} >
+                <button className="mecmovie-pagination-button" 
+                disabled={page.first} onClick={() => onChange(page.number - 1)} >
                     <Arrow />
                 </button>
-                <p>{`${1} de ${3}`}</p>
-                <button className="mecmovie-pagination-button" disabled={false} >
+                <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+                <button className="mecmovie-pagination-button" 
+                disabled={page.last}  onClick={() => onChange(page.number + 1)} >
                     <Arrow className="mecmovie-flip-horizontal" />
                 </button>
             </div>
